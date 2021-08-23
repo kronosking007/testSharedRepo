8d661fa1665143fcf74c54282520cf3aa764b391
Aditya <aditya.k.nath@gmail.com>
2021-08-23 10:46:00 +0930

    Add TestComponent4 to the project file

---
+++ src/components/TestComponent4.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TestComponent4 = () => {
+    return (
+        <div>
+            This is Test Component 4. This is a test File
+        </div>
+    )
+}
+
+export default TestComponent4
