0db185f9de068513ac422b7461e07fea2a796a49
Aditya <aditya.k.nath@gmail.com>
2021-08-23 10:43:03 +0930

    Add TestComponent3 to the project file

---
+++ src/components/TestComponent3.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TestComponent3 = () => {
+    return (
+        <div>
+            TestComponent 3 Has tjsi feature
+        </div>
+    )
+}
+
+export default TestComponent3
